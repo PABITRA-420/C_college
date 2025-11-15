8ec34d218a357ae532214052ac329410ff177f8d
Pabitra Sarkar <pabitrasarkar2003@outlook.com>
2025-11-15 21:22:06 +0530

    Add new file

---
+++ leapyear1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// leap year checker using conditional operator
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    (year % 4 == 0) ? 
+        ((year % 100 == 0) ? 
+            ((year % 400 == 0) ? 
+                printf("%d is a leap year.\n", year) : 
+                printf("%d is not a leap year.\n", year)) : 
+            printf("%d is a leap year.\n", year)) : 
+        printf("%d is not a leap year.\n", year);
+    return 0;
+}
---
+++ quadroot.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//root of a quadratic equation where coefficient are given by user
+int main() {
+    
+return 0;
+}
