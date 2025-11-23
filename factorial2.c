04540d895ce14ae597db1476aa7952172ea5ceec
Pabitra Sarkar <pabitrasarkar2003@outlook.com>
2025-11-24 00:09:59 +0530

    add new

--- factorial2.c
+++ factorial2.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
     int f,i,n;
-    printf("Enter a number\n");
+    printf("Enter a number :\n");
     scanf("%d",&n);
     f=1;
     i=1;
    do{
     f=f*i;
     i=i+1;//i++
-}while(i<=n)
+}while(i<=n);
     printf("ans is %d\n",f);
     return 0;
 }
